2dad3241b8569b46cd1c1c1c04eb6ce9ddf02d7a
JonathanGarciaG <36901381+JonathanGarciaG@users.noreply.github.com>
2020-05-27 08:30:57 -0500

    Practica 25/05/2020 controller
    ...

--- Practica-25-05-2020/controllers/controller.php
+++ Practica-25-05-2020/controllers/controller.php
@@ -36,11 +36,15 @@
 							</div>
 							<div class="form-group">
 								<label>Apellido: </label>
-								<input class="form-control" type="text" name="nusuariotxt" placeholder="Ingrese nombre" required>
+								<input class="form-control" type="text" name="ausuariotxt" placeholder="Ingrese nombre" required>
+							</div>
+							<div class="form-group">
+								<label>Usuario: </label>
+								<input class="form-control" type="text" name="usuariotxt" placeholder="Ingrese nombre" required>
 							</div>
 							<div class="form-group">
 								<label>Contraseña: </label>
-								<input class="form-control" type="text" name="nusuariotxt" placeholder="Ingrese nombre" required>
+								<input class="form-control" type="text" name="ucontratxt" placeholder="Ingrese nombre" required>
 							</div>
 							<div class="form-group">
 								<label>Correo Electronico: </label>
@@ -53,6 +57,88 @@
 			</div>
 			<?php
 		}
+
+		public function insertarUserController(){
+			if (isset($_POST["nusuariotxt"])) {
+				
+				$_POST["ncontratxt"] = password_hash($_POST["ncontratxt"],PASSWORD_DEFAULT);
+
+				$datosController = array["nusuario"=>$_POST["nusuariotxt"],"ausuario"=>$_POST["ausuariotxt"],"usuario"=>$_POST["usuariotxt"],"contra"=>$_POST["ucontratxt"],"email"=>$_POST["uemailtxt"];
+
+				$respuesta = Datos::insertarUserModel($datosController,"users");
+
+				if ($respuesta == "success") {
+					echo '
+						<div class="col-md-6 mt-3">
+							<div class="alert alert-success alert-dismissible">
+								<button>x</button>
+								<h5>
+									<i class="icon">
+									Exito
+								</h5>
+								Usuario agregado con exito
+							</div>
+						</div>
+						';
+				}else{
+					echo '
+						<div class="col-md-6 mt-3">
+							<div class="alert alert-success alert-dismissible">
+								<button>x</button>
+								<h5>
+									<i class="icon">
+									Error
+								</h5>
+								Se ha producido un error al momento de agregar un usuario
+							</div>
+						</div>
+					';
+				}
+			}
+		}
+
+		public function editarUserController(){
+			$datosController = $_GET["idUserEditar"];
+
+			$respuesta = Datos::editarUserModel($datosController,"users");
+			?>
+            <div class="col-md-6 mt-3">
+                <div class="card card-warning">
+                    <div class="card-header">
+                        <h4><b>Editor</b> de Usuarios</h4>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="index.php?action=usuarios">
+                            <div class="form-group">
+                                <input type="hidden" name="idUserEditar" class="form-control" value="<?php echo $respuesta["id"]; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="nusuariotxtEditar">Nombre: </label>
+                                <input class="form-control" type="text" name="nusuariotxtEditar" id="nusuariotxtEditar" placeholder="Ingrese el nuevo nombre" value="<?php echo $respuesta["nusuario"]; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="ausuariotxtEditar">Apellido: </label>
+                                <input class="form-control" type="text" name="ausuariotxtEditar" id="ausuariotxtEditar" placeholder="Ingrese el nuevo apellido" value="<?php echo $respuesta["ausuario"]; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="usuariotxtEditar">Usuario: </label>
+                                <input class="form-control" type="text" name="usuariotxtEditar" id="usuariotxtEditar" placeholder="Ingrese el nuevo usuario" value="<?php echo $respuesta["usuario"]; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="contratxtEditar">Contraseña: </label>
+                                <input class="form-control" type="password" name="contratxtEditar" id="contratxtEditar" placeholder="Ingrese la nueva contraseña" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="uemailtxtEditar">Correo Electrónico: </label>
+                                <input class="form-control" type="email" name="uemailtxtEditar" id="uemailtxtEditar" placeholder="Ingrese el nuevo correo electrónico" value="<?php echo $respuesta["email"]; ?>" required>
+                            </div>
+                            <button class="btn btn-primary" type="submit">Editar</button>
+                        </form>
+                    </div>
+                    </div>
+            </div>
+            <?php
+        }
 
 		// REGISTRO DE USUARIOS
 		public function registrarUsuarioController(){
