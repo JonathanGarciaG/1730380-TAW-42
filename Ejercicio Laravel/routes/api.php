e2b62ea2aca16c8eeef5431e43df29d81fc26623
JonathanGarciaG <36901381+JonathanGarciaG@users.noreply.github.com>
2020-06-22 08:42:42 -0500

    Update api.php
    Rutas CRUD

--- Ejercicio Laravel/routes/api.php
+++ Ejercicio Laravel/routes/api.php
@@ -55,3 +55,34 @@ Route::post('empleados',function(Request $request){
 
 });
 
+//Ruta para actualizar empleado
+Route::put('empleados/{id}',function(Request $request, $id){
+	$request->validate([
+		'nombres'=>'required|max:50',
+		'apellidos'=>'required|max:50',
+		'cedula'=>'required|max:20',
+		'email'=>'required|max:50|email|unique:empleados,email, '. $id,
+		'lugar_nacimiento'=>'required|max:50',
+		'estado_civil'=>'required|max:50',
+		'telefono'=>'required|numeric'
+	]);
+	$empleado = Empleado::findOrFail($id);
+	$empleado->nombres = $request->input('nombres');
+	$empleado->apellidos = $request->input('apellidos');
+	$empleado->cedula = $request->input('cedula');
+	$empleado->email = $request->input('email');
+	$empleado->lugar_nacimiento = $request->input('lugar_nacimiento');
+	$empleado->sexo = $request->input('sexo');
+	$empleado->estado_civil = $request->input('estado_civil');
+	$empleado->telefono = $request->input('telefono');
+	$empleado->save();
+	return "Empleado actualizado";
+});
+
+
+//Ruta para eliminar empleado
+Route::delete('empleados/{id}',function($id){
+	$empleado = Empleado::findOrFail($id);
+	$empleado -> delete();
+	return "Empleado eliminado";
+});
